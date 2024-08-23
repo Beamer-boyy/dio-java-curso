9a24a1a05d1c1b479736ba369a684794cbd63693
Luis Santos <luishenriquessantos360@gmail.com>
2024-08-23 00:15:41 -0300

    primeiro commit

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -1,6 +1,6 @@
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Seja bem vindo");
-        System.out.println("o que voce desgdfdfdfeja hoje?");
+        System.out.println("o que voce deseja fazer hoje?");
     }
 }
