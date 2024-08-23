a18b55fbff3443043a0447db64fac0b90c541602
Luis Santos <luishenriquessantos360@gmail.com>
2024-08-23 00:17:14 -0300

    atualizando

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -2,5 +2,6 @@ public class contaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Seja bem vindo");
         System.out.println("o que voce deseja fazer hoje?");
+        System.out.println("estamos felizes em velo de novo");
     }
 }
